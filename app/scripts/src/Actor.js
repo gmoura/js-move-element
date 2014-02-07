4ee5076bee4b2fc83550b7e873ac1b4d884b533e
Guilherme Moura Nascimento <gmoura.nas@gmail.com>
2014-02-06 22:29:17 -0200

    Rotate Actor

--- app/scripts/src/Actor.js
+++ app/scripts/src/Actor.js
@@ -13,7 +13,7 @@ function Actor () {
  */
 Actor.prototype.getAxis = function(axis) {
   if(axis === "x"){
-    return this.element.offsetLeft + this.element.offsetWidth;
+    return this.element.offsetLeft + ( this.element.offsetWidth / 2 );
   }
   if(axis === "y"){
     return this.element.offsetTop;
@@ -22,24 +22,48 @@ Actor.prototype.getAxis = function(axis) {
 
 Actor.prototype.moveShortToTop = function() {
   var increase = this.getAxis('y') - 5;
+  
+  this.changeClass('rotate-to-up');
   this.element.style.top = increase + "px";
 };
 
 Actor.prototype.moveShortToDown = function() {
   var decrements = this.getAxis('y') + 5 ;
-  this.element.style.top = decrements + "px";
 
+  this.changeClass('rotate-to-down');
+  this.element.style.top = decrements + "px";
 };
 
 Actor.prototype.moveShortToLeft = function() {
-  var increase = this.getAxis('x') - 1 - this.element.offsetWidth; 
-  this.element.style.left = increase + "px";
+  var increase = this.getAxis('x') - 5; 
 
+  this.changeClass('rotate-to-left');
+  this.element.style.left = increase + "px";
 };
 
 Actor.prototype.moveShortToRight = function() {
-  var decrements = this.getAxis('x') + 1 ;
+  var decrements = this.getAxis('x') + 5;
+
+  this.changeClass('rotate-to-right');
   this.element.style.left = decrements + "px";
+};
+
+Actor.prototype.changeClass = function(newValue) {
+  var 
+    classReg = new RegExp(newValue),
+    oldClass = this.element.className,
+    newClass = ""; 
+
+    //Verify if newClass existe
+    if( ! classReg.test(oldClass) ) {
+      //Remove All class with started 'rotate-'
+      newClass = oldClass.replace(/ rotate.*/,"");
+
+      //New string class
+      newClass += " " + newValue;
+      
+      this.element.className = newClass;
+    }
 
 };
 
